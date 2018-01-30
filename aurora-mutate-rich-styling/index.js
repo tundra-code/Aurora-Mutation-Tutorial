b5a6edd91fee5fdc46af8ff71efc9a1089950a48
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2018-01-29 20:50:56 -0800

    Wrote a real rich styling mutation this time

--- aurora-mutate-rich-styling/index.js
+++ aurora-mutate-rich-styling/index.js
@@ -1,19 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { RichUtils } from "draft-js";
 
-function RichStyling(Frame) {
+function RichStyling(Editor) {
   return class extends React.Component {
     constructor(props) {
       super(props);
-      window.styling = true;
+      this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    }
+
+    // rich styling here
+    handleKeyCommand(command, editorState) {
+      const newState = RichUtils.handleKeyCommand(editorState, command);
+      if (newState) {
+        this.props.onChangeEx(newState);
+        return "handled";
+      }
+      return "not-handled";
     }
 
     render() {
-      return <Frame>{this.props.children}</Frame>;
+      return (
+        <Editor {...this.props} handleKeyCommand={this.handleKeyCommand}>
+          {this.props.children}
+        </Editor>
+      );
     }
   };
 }
 
 module.exports.mutations = {
-  Frame: RichStyling
+  BaseEditor: RichStyling
 };
